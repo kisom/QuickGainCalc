eb083d3250e18299b4004fbc5d77e08e57e7b9dd
Kyle Isom <coder@kyleisom.net>
2011-08-19 17:57:12 +0300

    Adding interface for StockDelta class.

---
+++ QuickGainCalc/KI_StockDelta.h
@@ -0,0 +1,27 @@
+//
+//  KI_StockDelta.h
+//  QuickGainCalc
+//
+//  Created by Kyle Isom on 8/19/11.
+//  Copyright 2011 Kyle Isom. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KI_StockDelta : NSObject
+{
+    double buyPrice, sellPrice;
+    int shares;
+    double yield, gain;                 // yield is % return: yield + 1 * 100 for calculations
+}
+
+@property double buyPrice, sellPrice;
+@property int shares;
+@property double yield, gain;
+
+- (void) calcYieldAndGainFromDelta;     // updates yield and gain
+- (void) calcSellFromYield;             // updates sellPrice and gain
+- (void) calcSellFromGain;              // updates sellPrice and yield
+
+
+@end
